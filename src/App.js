cc94c1e1563060e4b601b0548ececba51c4c057d
1cekrim <1cekrim.dev@gmail.com>
2021-01-30 14:14:57 +0900

    [add] react-router-dom 테스트 추가

--- src/App.js
+++ src/App.js
@@ -1,10 +1,31 @@
 import './App.css';
+import { BrowserRouter, Link, Route } from "react-router-dom"
 
 function App() {
   return (
     <div className="App">
-      <header className="App-header">
-      </header>
+      <BrowserRouter>
+        <header className="App-header">
+          <Link to="/">
+            <button>Home</button>
+          </Link>
+          <Link to="/about">
+            <button>About</button>
+          </Link>
+          <Link to="/services">
+            <button>Services</button>
+          </Link>
+          <Link to="/awards">
+            <button>Awards</button>
+          </Link>
+          <Link to="/board">
+            <button>Board</button>
+          </Link>
+        </header>
+        <main>
+          <Route exact path="/" />
+        </main>
+      </BrowserRouter>
     </div>
   );
 }
